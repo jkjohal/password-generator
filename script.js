c552b317633f5a64f57162691c6e9235660e76e8
Jeevan Johal <jkjohal@ucdavis.edu>
2023-02-03 14:01:00 -0800

    add pw criteria to HTML doc and edited Javascript functions

--- script.js
+++ script.js
@@ -1,54 +1,42 @@
 // Assignment code here
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-
 //password criteria
 //length (8-128), lowercase, uppercase, numeric, special characters, need at least one type
-var passwordLength = window.prompt("How many characters do you want your password to be?");
 
-var passwordLowerCase = window.confirm("Do you want your password to include lower case letters?");
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const numbers = "0123456789";
+const symbols = "!@#$%^&*_-+=";
 
-if(passwordLowerCase){
-  let lowerCaseChars = "abcdefghijklmnopqrstuvwxyz"
-} else {
-  let lowerCaseChars = ""
-};
-
-var passwordUpperCase = window.confirm("Do you want your password to include upper case letters?");
-
-if(passwordUpperCase){
-  let upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-} else {
-  let upperCaseChars = ""
-};
-
-var passwordNumbers = window.confirm("Do you want your password to include numbers?");
-
-if(passwordNumbers){
-  let numberChars = "0123456789"
-} else {
-  let numberChars = ""
-};
-
-var passwordSpecial = window.confirm("Do you want your password to include special characters?");
-
-if(passwordSpecial){
-  let specialChars = "!@#$%^&*()"
-} else {
-  let specialChars = ""
-};
+// Get references to the #generate element
+const generateBtn = document.querySelector("#generate");
+const passwordText = document.querySelector("#password");
+const length = document.querySelector("#length");
+const includeUppercase = document.querySelector("#uppercase");
+const includeLowercase = document.querySelector("#lowercase");
+const includeNumbers = document.querySelector("#numbers");
+const includeSymbols = document.querySelector("#symbols");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function passwordCriteria() {
+  let characters = "";
+  includeUppercase.checked ? (characters += uppercase) : "";
+  includeLowercase.checked ? (characters += lowercase) : "";
+  includeNumbers.checked ? (characters += numbers) : "";
+  includeSymbols.checked ? (characters += symbols) : "";
+  passwordText.value = generatePassword(length.value, characters);
+};
 
-  passwordText.value = password;
-
-}
+function generatePassword(length, characters){
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  return password;
+};
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", passwordCriteria);
