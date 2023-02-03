0414c11c327176e4b52550979f5b157c4ddf5050
Jeevan Johal <jkjohal@ucdavis.edu>
2023-02-03 15:22:55 -0800

    completed challenge

--- script.js
+++ script.js
@@ -5,12 +5,13 @@
 //length (8-128), lowercase, uppercase, numeric, special characters, need at least one type
 
 const lowercase = "abcdefghijklmnopqrstuvwxyz";
-const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*_-+=";
 
 // Get references to the #generate element
 const generateBtn = document.querySelector("#generate");
+const showPassword = document.querySelector("#show-password")
 const passwordText = document.querySelector("#password");
 const length = document.querySelector("#length");
 const includeUppercase = document.querySelector("#uppercase");
@@ -38,5 +39,18 @@ function generatePassword(length, characters){
   return password;
 };
 
+function showCriteria(){
+  var x = document.querySelector("#password-settings");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+};
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", passwordCriteria);
+showPassword.addEventListener("click", passwordCriteria);
+
+generateBtn.addEventListener("click", showCriteria);
+
+
